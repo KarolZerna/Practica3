c57ab2434ab7fac8db4835ae95ed2d9b64544971
KarolZerna <karol.zern@gmail.com>
2021-11-12 23:55:33 -0400

    testing

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+import alarmexception
+import board
 
+class TestAlarmException(unittest.TestCase):
     def alarm_exception(self):
         pass
 
+# class TestBoard(unittest.TestCase):
+#     def test_game_board(self):
+#         self.assertEqual(board.gameArray[0,0], 1)
 
 if __name__ == '__main__':
     unittest.main()
