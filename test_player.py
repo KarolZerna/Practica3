ba70972af9a9b0b1344db271267675140898feb7
KarolZerna <karol.zern@gmail.com>
2021-11-13 04:13:21 -0400

    player init

--- test_player.py
+++ test_player.py
@@ -11,7 +11,6 @@ class TestPlayer(unittest.TestCase):
         self.lives = 3
         self.x = 3 #19
         self.y = 1 #5
-        self.update = 3
     
     def test_drawPlayer(self):
         self.assertEqual(Player.drawPlayer(self, self.x, self.y), None)
@@ -25,6 +24,8 @@ class TestPlayer(unittest.TestCase):
     def test_checkEnemy(self):
         self.assertEqual(Player.checkEnemy(self, self.x, self.y), 1)
 
+    def test_playerInit(self):
+        self.assertEqual(Player.playerInit(self), None)
 
         
 if __name__ == '__main__':
