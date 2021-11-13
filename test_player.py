fc85c3480b274b6bd6d8048ba541c3be182614ec
Daniel Acero <edanielacero@gmail.com>
2021-11-13 03:01:57 -0400

    test_player added

---
+++ test_player.py
@@ -0,0 +1,9 @@
+import unittest
+from player import Player
+
+class TestPlayer(unittest.TestCase):
+     def test_player(self):
+        self.assertEqual(, )
+        
+if __name__ == '__main__':
+    unittest.main()
