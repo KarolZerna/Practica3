6dbfcc317aaa41c8846ff165988f25ab7ca768a5
Mercedes <mercedes.htcv@gmail.com>
2021-11-13 03:29:48 -0400

    checkpost from bomb.py

--- test_bomb.py
+++ test_bomb.py
@@ -1,6 +1,10 @@
 import unittest
 from bomb import Bomb
 
+class TestBomb(unittest.TestCase):
+    def test_check_post(self):
+         self.assertEqual(Bomb.checkPos(self, 3, 1), 1)
+    
 
 if __name__ == '__main__':
     unittest.main()
