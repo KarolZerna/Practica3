a268eae6ba2d4c9b066abc1a075fc9e7c11d75da
Mercedes <mercedes.htcv@gmail.com>
2021-11-13 01:09:01 -0400

    Person.py complete

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 
 import alarmexception
-import board
+import board, person
 
 class TestAlarmException(unittest.TestCase):
     def alarm_exception(self):
@@ -11,5 +11,8 @@ class TestAlarmException(unittest.TestCase):
 #     def test_game_board(self):
 #         self.assertEqual(board.gameArray[0,0], 1)
 
+class Person(unittest.TestCase):
+     def TestCheckPost(self):
+         self.assertEqual(person.checkPos(3,60),-1)
 if __name__ == '__main__':
     unittest.main()
