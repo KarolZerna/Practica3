dd03a28a1c83fab2291eff52a3eee2d3b64b0827
KarolZerna <karol.zern@gmail.com>
2021-11-13 01:23:51 -0400

    test board

--- test.py
+++ test.py
@@ -7,15 +7,20 @@ class TestAlarmException(unittest.TestCase):
     def alarm_exception(self):
         pass
 
-# class TestBoard(unittest.TestCase):
-#     def test_game_board(self):
-#         self.assertEqual(board.gameArray[0,0], 1)
+class TestBoard(unittest.TestCase):
+    gameArray = [[0 for x in range(80)] for y in range(40)]
+    gameArray[0][0]=1
 
-class Person(unittest.TestCase):
+    def test_game_board(self):
+        # gameArray[0][0]=1
+        self.assertEqual(board.gameArray[2][37],"X")
+
+class TestPerson(unittest.TestCase):
      def TestCheckPost(self):
          self.assertEqual(person.checkPos(3,60),-1)
          self.assertEqual(person.checkPos(40,60),-1)
          self.assertEqual(person.checkPos(3,1),-1)
          self.assertEqual(person.checkPos(3,80),-1)
+         
 if __name__ == '__main__':
     unittest.main()
