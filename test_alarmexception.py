49c5467776e134153c08dad0c67dec3ec8c6fdbe
KarolZerna <karol.zern@gmail.com>
2021-11-13 02:59:33 -0400

    alarm exception

---
+++ test_alarmexception.py
@@ -0,0 +1,9 @@
+import unittest
+from alarmexception import AlarmException
+
+class TestAlarmException(unittest.TestCase):
+     def test_alarmexceptio(self):
+        #  self.assertEqual(, )
+        
+if __name__ == '__main__':
+    unittest.main()
