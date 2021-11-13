9da271c70523a2ccdad3860358e77547bc633e52
KarolZerna <karol.zern@gmail.com>
2021-11-13 03:25:22 -0400

    2 new unit test player

--- test_player.py
+++ test_player.py
@@ -1,9 +1,23 @@
 import unittest
+from person import * 
+from board import *
 from player import Player
 
 class TestPlayer(unittest.TestCase):
-     def test_player(self):
-        self.assertEqual(, )
+
+    def setUp(self):
+        self.playerPos = [1,1]
+        self.score = 0
+        self.lives = 3
+        self.x = 19
+        self.y = 5
+
+    def test_checkPos_pos(self):
+        self.assertEqual(Player.checkPos(self, self.x, self.y), 1)
+
+    def test_checkPos_neg(self):
+        self.assertIsNot(Player.checkPos(self, self.x, self.y), -1)
+
         
 if __name__ == '__main__':
     unittest.main()
