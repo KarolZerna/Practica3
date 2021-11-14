ba42604f632042229cab1b8a1c118b337181a03e
KarolZerna <karol.zern@gmail.com>
2021-11-14 16:57:08 -0400

    person test

--- test_bomb.py
+++ test_bomb.py
@@ -14,21 +14,33 @@ class TestBomb(unittest.TestCase):
         self.x = 3 #19
         self.y = 1 #5
         self.playerPos= []
+        self.gameArray = [[0 for x in range(80)] for y in range(40)]
+        self.gameArray[0][0]=1
 
 
     def test_drawBomb(self):
-        pos = self.bomPos[2]
         self.assertEqual(Bomb.drawBomb(self), None)
         # self.assertGreater(Bomb.drawBomb(pos), self.x)
+        # self.assertFalse(Bomb.drawBomb(self.bimbo))
+    
+    def test_drawBomb_not_bimbo(self):
         self.assertIsNone(Bomb.drawBomb(self.bimbo))
-        self.assertFalse(Bomb.drawBomb(self.bimbo))
-        
+
 
     def test_drawExplosion(self):
         self.assertEqual(Bomb.drawExplosion(self, self.x, self.y), None)
 
     def test_check_post(self):
         self.assertEqual(Bomb.checkPos(self, 3, 1), 1)
+
+    def test_check_pos_explosion(self):
+        if(Bomb.checkPos(self, self.x-1, self.y) > 0):
+            self.assertIsNone(Bomb.drawExplosion(self, self.x-1,self.y))
+
+        if(self.gameArray[2*self.x+1][4*self.y+1]=="X"):
+            # self.assertIs(-1)
+            self.assertEqual(Bomb.drawExplosion(self, self.x-1,self.y), -1)
+
         # self.assertEqual(Bomb.checkPos(self, [2*self.x+1], [4*self.y+1]), "X")
 
 
@@ -41,9 +53,9 @@ class TestBomb(unittest.TestCase):
 
     def test_explosion(self):
         self.assertEqual(Bomb.explosion(self), None)
-        self.assertEqual(Bomb.drawExplosion(self, self.x, self.y), None)
-        self.assertEqual(Bomb.afterExplosion(self, self.x, self.y), None)
-        self.assertNotEqual(Bomb.checkPos(self, 3, 1), -1)
+        # self.assertEqual(Bomb.drawExplosion(self, self.x, self.y), None)
+        # self.assertEqual(Bomb.afterExplosion(self, self.x, self.y), None)
+        # self.assertNotEqual(Bomb.checkPos(self, 3, 1), -1)
 
 if __name__ == '__main__':
     unittest.main()
--- test_person.py
+++ test_person.py
@@ -2,8 +2,12 @@ import unittest
 from person import Person
 
 class TestPersonCheck(unittest.TestCase):
-     def test_check_post_mayor(self):
-         self.assertEqual(Person.checkPos(self, 3, 1), -1)
+    def test_check_post_mayor(self):
+        self.assertEqual(Person.checkPos(self, 3, 1), -1)
+
+    def test_check_pos(self):
+        self.assertEqual(Person.checkPos(self, 68, 70), -1)
+    
         
 if __name__ == '__main__':
     unittest.main()
