0ace95b36c9dc8ca60e532f87bf4a70620e20a3e
KarolZerna <karol.zern@gmail.com>
2021-11-14 15:17:25 -0400

    test_board

--- test_board.py
+++ test_board.py
@@ -1,10 +1,15 @@
 import unittest
+from unittest.case import expectedFailure
 from board import Board
 
-class TestBoard(unittest.TestCase):
-    def test_game_board(self):
-        self.assertEqual()
+class TestPersonCheck(unittest.TestCase):
 
+     def test_gameBoard(self):
+        gameArray = [[0 for x in range(80)] for y in range(40)]
+        gameArray[0][0]=1
+        expected = gameArray
+        self.assertEqual(Board.gameBoard(self), expected)
 
+        
 if __name__ == '__main__':
     unittest.main()
