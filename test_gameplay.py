20ef77da729fd9dba0c6fcc28984076025b3ec3b
KarolZerna <karol.zern@gmail.com>
2021-11-14 17:34:37 -0400

    gameplay return

--- test_gameplay.py
+++ test_gameplay.py
@@ -3,36 +3,27 @@ import time
 from gameplay import Gameplay
 from unittest import mock
 
-from board import *
-from brick import *
-from person import *
-from player import *
-from enemy import *
-from bomb import *
-bo = Board()
-br = Brick()
-pl = Player()
-en = Enemy()
-bom = Bomb()
-
 
 class TestGamePlay(unittest.TestCase):
 
     def setUp(self):
-        self.gameArray = bo.gameBoard()
-		# self.br = br.drawBricks()
-		# self.pl = pl.updatePlayer()
         self.cur_time = time.time()
-        self.run_time = self.cur_time
-	    # self.run_time = time.time() - self.cur_time
+        # self.run_time = time.time() - self.cur_time
 
     def test_init(self):
         self.assertNotEqual(Gameplay.__init__(self.cur_time), time.time())
 
 
     def test_printboard(self):
-        # self.assertNotEqual(self.run_time, time.time()-self.cur_time)
-        pass
+        self.assertGreater(Gameplay.printboard(time.time() - self.cur_time), 1)
+        for x in range(1, 39):
+            with self.subTest(x = x):
+                # self.assertEqual(1 // x, 0)
+                self.assertNotEqual(Gameplay.__init__(self.cur_time), time.time())
+
+    def test_printboard_return(self):
+        self.assertEqual(Gameplay.printboard(self), None)
+
 
 if __name__ == '__main__':
     unittest.main()
